beac903747984480a99108276f585f9cdfa47f97
Dawid Olczak <dawid.a.olczak@gmail.com>
2025-03-28 23:01:49 +0100

    kl

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-    API_URL: 'https://api.bigsewciu.shop',
-    // API_URL: 'http://localhost:3002',
+    // API_URL: 'https://api.bigsewciu.shop',
+    API_URL: 'http://localhost:3002',
     REACT_APP_LOGOUT: 'api/auth/logout',
 }
