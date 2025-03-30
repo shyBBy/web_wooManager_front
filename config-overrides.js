05d4b3d2a73922587323af40ddbaa40a83a523f9
Dawid Olczak <dawid.a.olczak@gmail.com>
2025-03-30 14:57:49 +0200

    usuniecie react-app-rewired

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,5 @@
-const {override} = require('customize-cra');
-const {aliasDangerous, configPaths} = require('react-app-rewire-alias/lib/aliasDangerous');
+const { override } = require('customize-cra');
 
 module.exports = function override(config) {
-  return config; // Usuń konfigurację aliasów, jeśli nie jest potrzebna
+  return config; // Usunięto konfigurację aliasów
 };
